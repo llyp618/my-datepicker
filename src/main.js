6c86d5b9f0a105dea42617f7c0435d1201f4f6f2
llyp618 <llyp618@163.com>
2017-11-16 13:49:29 +0800

    remove dist

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
-export class Dialog {
-  constructor(config){
-    this.defaultConfig = {
-      name:'dialog'
-    }
-    this.name = (config && config.name) || this.defaultConfig.name
-  }
-  say(){
-    console.log(this.name)
-  }
-}
+// style
+import './main.less'
+
+
+
 
